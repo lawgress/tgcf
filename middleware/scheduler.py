6b35e5ae0e3595342f3e61ff3c6965e64b212cca
lawgress <166209004+lawgress@users.noreply.github.com>
2025-05-27 16:56:56 +0330

    Create scheduler.py

---
+++ middleware/scheduler.py
@@ -0,0 +1,19 @@
+from .retailer_scraper import get_argos_products, get_currys_products
+from .ebay_api import get_ebay_resale_data
+from .filter_logic import calculate_profit
+from tgcf.plugins.sender import send_message
+
+async def scan_and_notify():
+    all_products = get_argos_products() + get_currys_products()
+    for product in all_products:
+        resale = get_ebay_resale_data(product["name"])
+        profit = calculate_profit(product["price"], resale)
+        if profit:
+            msg = (
+                f"🔥 *{product['name']}*\n"
+                f"[Buy Now]({product['url']})\n"
+                f"Retail: £{product['price']}\n"
+                f"Avg. Resell: £{round(sum(resale)/len(resale), 2)}\n"
+                f"Estimated Profit: *£{profit}*"
+            )
+            await send_message(msg, markdown=True)
