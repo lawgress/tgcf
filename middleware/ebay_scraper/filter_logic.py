5dc8382a394739cefc0d802606db0291bd9513f4
lawgress <166209004+lawgress@users.noreply.github.com>
2025-05-27 09:53:56 -0700

    Update filter_logic.py

--- middleware/ebay_scraper/filter_logic.py
+++ middleware/ebay_scraper/filter_logic.py
@@ -3,6 +3,6 @@ def calculate_profit(retail_price, resale_prices):
         return None
     avg_resale = sum(resale_prices) / len(resale_prices)
     profit = avg_resale - retail_price
-    if avg_resale > 2 * retail_price:
+    if avg_resale > retail_price:
         return round(profit, 2)
     return None
