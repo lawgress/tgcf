a418b098f5a4372778c9e2349492cf50228fafa4
lawgress <166209004+lawgress@users.noreply.github.com>
2025-05-28 10:54:13 +0330

    Update __init__.py

--- middleware/__init__.py
+++ middleware/__init__.py
@@ -1,1 +1,10 @@
 
+"""Package tgcf.
+
+The ultimate tool to automate custom telegram message forwarding.
+https://github.com/aahnik/tgcf
+"""
+
+from importlib.metadata import version
+
+__version__ = version(__package__)
