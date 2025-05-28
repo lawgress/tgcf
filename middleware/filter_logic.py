35cd233fe20981ef3f8f9ea9038a12de19e50d4f
lawgress <166209004+lawgress@users.noreply.github.com>
2025-05-28 15:59:15 +0330

    Update filter_logic.py

--- middleware/filter_logic.py
+++ middleware/filter_logic.py
@@ -1,8 +1,13 @@
-def calculate_profit(retail_price, resale_prices):
-    if not resale_prices:
-        return None
-    avg_resale = sum(resale_prices) / len(resale_prices)
-    profit = avg_resale - retail_price
-    if avg_resale > retail_price:
-        return round(profit, 2)
-    return None
+#def calculate_profit(retail_price, resale_prices):
+#    if not resale_prices:
+#        return None
+#    avg_resale = sum(resale_prices) / len(resale_prices)
+#    profit = avg_resale - retail_price
+#    if avg_resale > retail_price:
+#        return round(profit, 2)
+#    return None
+
+
+def is_profitable(product):
+    return True  # Always treat as profitable during testing
+
