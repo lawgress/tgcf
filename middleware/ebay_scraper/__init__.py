e8b5ef62f510c24d0480ef20f8667c70de9bdedb
lawgress <166209004+lawgress@users.noreply.github.com>
2025-05-28 14:06:36 +0330

    Delete middleware/ebay_scraper directory

--- middleware/ebay_scraper/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Package tgcf.
-
-The ultimate tool to automate custom telegram message forwarding.
-https://github.com/aahnik/tgcf
-"""
-
-from importlib.metadata import version
-
-__version__ = version(__package__)
