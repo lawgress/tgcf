640e57fe727afb04b1e65ff286d442c39d6768bf
lawgress <166209004+lawgress@users.noreply.github.com>
2025-05-28 10:51:38 +0330

    Delete ebay_scraper directory

--- ebay_scraper/filter_logic.py
+++
@@ -1,8 +0,0 @@
-def calculate_profit(retail_price, resale_prices):
-    if not resale_prices:
-        return None
-    avg_resale = sum(resale_prices) / len(resale_prices)
-    profit = avg_resale - retail_price
-    if avg_resale > 2 * retail_price or profit > 50:
-        return round(profit, 2)
-    return None
